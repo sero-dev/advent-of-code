09c9ad25fd71f74d16115e71f960a1df01dfb9e0
Sean Rodriguez <sean.rodriguez@outlook.com>
2023-12-02 02:50:40 -0500

    Add partial solution for 2023 - Day 2 - Puzzle 2

--- src/app/solutions/2023/day-01/trebuchet.service.spec.ts
+++ src/app/solutions/2023/day-01/trebuchet.service.spec.ts
@@ -43,4 +43,77 @@ describe('TrebuchetService', () => {
     const result = service.puzzle1(input);
     expect(result).toEqual(142);
   });
+
+  it('should return calibration+ value of 29 for two1nine', () => {
+    const input = 'two1nine';
+    const result = service.getCalibrationValuePlus(input);
+    expect(result).toEqual(29);
+  });
+
+  it('should return calibration+ value of 83 for eightwothree', () => {
+    const input = 'eightwothree';
+    const result = service.getCalibrationValuePlus(input);
+    expect(result).toEqual(83);
+  });
+
+  it('should return calibration+ value of 13 for abcone2threexyz', () => {
+    const input = 'abcone2threexyz';
+    const result = service.getCalibrationValuePlus(input);
+    expect(result).toEqual(13);
+  });
+
+  it('should return calibration+ value of 24 for xtwone3four', () => {
+    const input = 'xtwone3four';
+    const result = service.getCalibrationValuePlus(input);
+    expect(result).toEqual(24);
+  });
+
+  it('should return calibration+ value of 42 for 4nineeightseven2', () => {
+    const input = '4nineeightseven2';
+    const result = service.getCalibrationValuePlus(input);
+    expect(result).toEqual(42);
+  });
+
+  it('should return calibration+ value of 29 for zoneight234', () => {
+    const input = 'zoneight234';
+    const result = service.getCalibrationValuePlus(input);
+    expect(result).toEqual(14);
+  });
+
+  it('should return calibration+ value of 29 for 7pqrstsixteen', () => {
+    const input = '7pqrstsixteen';
+    const result = service.getCalibrationValuePlus(input);
+    expect(result).toEqual(76);
+  });
+
+  it('should return total for all inputs in puzzle 2', () => {
+    const input =
+      'two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen';
+    const result = service.puzzle2(input);
+    expect(result).toEqual(281);
+  });
+
+  it('should convert ONE to 1', () => {
+    const input = 'ONE';
+    const result = service.convertWordToNumber(input);
+    expect(result).toEqual(1);
+  });
+
+  it('should convert two to 2', () => {
+    const input = 'two';
+    const result = service.convertWordToNumber(input);
+    expect(result).toEqual(2);
+  });
+
+  it('should convert EiGhT to 8', () => {
+    const input = 'EiGhT';
+    const result = service.convertWordToNumber(input);
+    expect(result).toEqual(8);
+  });
+
+  it('should convert nAn to undefined', () => {
+    const input = 'nAn';
+    const result = service.convertWordToNumber(input);
+    expect(result).toEqual(undefined);
+  });
 });
--- src/app/solutions/2023/day-01/trebuchet.service.ts
+++ src/app/solutions/2023/day-01/trebuchet.service.ts
@@ -13,8 +13,12 @@ export class TrebuchetService implements ProblemService {
     );
   }
 
-  puzzle2(input: string) {
-    return '';
+  puzzle2(input: string): number {
+    const lines = input.split('\n').filter((line) => line.trim().length !== 0);
+    return lines.reduce(
+      (total, line) => total + this.getCalibrationValuePlus(line),
+      0
+    );
   }
 
   getCalibrationValue(line: string): number {
@@ -28,4 +32,40 @@ export class TrebuchetService implements ProblemService {
 
     return Number(`${matches[0]}${matches[matches.length - 1]}`);
   }
+
+  getCalibrationValuePlus(line: string): number {
+    const matches = line.match(
+      /(one|two|three|four|five|six|seven|eight|nine|\d)/g
+    );
+
+    if (!matches) {
+      throw new Error(
+        `Input string '${line}' does not contain a numeric character`
+      );
+    }
+
+    const firstMatch = matches[0];
+    const lastMatch = matches[matches.length - 1];
+
+    const firstNumber = this.convertWordToNumber(firstMatch) ?? firstMatch;
+    const lastNumber = this.convertWordToNumber(lastMatch) ?? lastMatch;
+
+    return Number(`${firstNumber}${lastNumber}`);
+  }
+
+  convertWordToNumber(input: string): number | undefined {
+    return NumberMap[input.toLowerCase()];
+  }
 }
+
+const NumberMap: { [key in string]: number } = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
